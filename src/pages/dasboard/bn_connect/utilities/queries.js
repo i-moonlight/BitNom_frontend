bdb8bbb291abc72028cf53db5df358d1f7ac4992
Jack Mkimbo <jackmkimbo@gmail.com>
2021-07-19 16:34:11 +0300

    initial commit

--- src/pages/dasboard/bn_connect/utilities/queries.js
+++ src/pages/dasboard/bn_connect/utilities/queries.js
@@ -8,3 +8,43 @@ export const MUTATION_CREATE_POST = gql`
     }
   }
 `;
+
+export const MUTATION_UPDATE_POST = gql`
+  mutation ($data: IUpdatePost!) {
+    Posts {
+      update(data: $data)
+    }
+  }
+`;
+
+export const MUTATION_DELETE_POST = gql`
+  mutation ($_id: ID!) {
+    Posts {
+      delete(_id: $_id)
+    }
+  }
+`;
+
+export const MUTATION_CREATE_COMMENT = gql`
+  mutation ($data: ICreateComment!) {
+    Comments {
+      create(data: $data)
+    }
+  }
+`;
+
+export const MUTATION_UPDATE_COMMENT = gql`
+  mutation ($data: IUpdateComment!) {
+    Comments {
+      update(data: $data)
+    }
+  }
+`;
+
+export const MUTATION_DELETE_COMMENT = gql`
+  mutation ($_id: ID!) {
+    Comments {
+      delete(_id: $_id)
+    }
+  }
+`;
