eb2dc32deec015d6d5397653aa5ba011ff3807bb
thecodeinfluencer <mbenjerminne@gmail.com>
2021-09-27 20:08:25 +0300

    remove carousel

--- src/test_component/TestComponent.js
+++ src/test_component/TestComponent.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import MuiCarousel from './MuiCarousel';
 
 export default function TestComponent() {
   return (
     <>
       {/* <TrainerCarousel /> */}
-      <MuiCarousel />
+      {/* <MuiCarousel /> */}
     </>
   );
 }
