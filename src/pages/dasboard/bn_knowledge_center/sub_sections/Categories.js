ebfbaa4cf7ac7aa3835e2b65d49ed7724ad97437
donphelix <margiomondi@gmail.com>
2021-11-05 01:58:52 +0300

    Modified knowledge center tabs

---
+++ src/pages/dasboard/bn_knowledge_center/sub_sections/Categories.js
@@ -0,0 +1,97 @@
+/**
+ * Created by PhpStorm.
+ * User: don@donphelix.com
+ * Date: 11/4/21
+ * Time: 9:06 PM
+ */
+
+
+import React, {useEffect} from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import {Card, LinearProgress} from '@mui/material';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+}));
+
+export default function Categories() {
+    const [categories, getCategories] = React.useState([]);
+    const [categoryLoaded, loadCategories] = React.useState(false);
+
+    useEffect(() => {
+        const url = `https://api.coingecko.com/api/v3/coins/categories`;
+        fetch(url)
+            .then(response =>response.json())
+            .then(data=>{
+                console.log(data);
+                getCategories(data);
+                loadCategories(true);
+            })
+            .catch(loadCategories(false));
+    },[]);
+
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ maxHeight: 500 }} aria-label='coins table' stickyHeader>
+                <TableHead>
+                    <TableRow>
+                        <StyledTableCell>#</StyledTableCell>
+                        <StyledTableCell>Name</StyledTableCell>
+                        <StyledTableCell>Market Cap</StyledTableCell>
+                        <StyledTableCell>Market Change (24h)</StyledTableCell>
+                        <StyledTableCell>Volume (24h)</StyledTableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {categoryLoaded ? (categories.map((row, id) => (
+                            <StyledTableRow key={row.name}>
+                                <StyledTableCell component='th' scope='row'>
+                                    {id + 1}
+                                </StyledTableCell>
+                                <StyledTableCell component='th' scope='row'>
+                                    {row.name}
+                                </StyledTableCell>
+                                <StyledTableCell>
+                                    ${row.market_cap}
+                                </StyledTableCell>
+                                <StyledTableCell className={'text-danger'}>
+                                    {row.market_cap_change_24h}%
+                                </StyledTableCell>
+                                <StyledTableCell>
+                                    ${row.volume_24h}
+                                </StyledTableCell>
+                            </StyledTableRow>
+                        )))
+                        :
+                        <Card className={'text-center'}>
+                            <LinearProgress color='inherit' />
+                        </Card>
+                    }
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
