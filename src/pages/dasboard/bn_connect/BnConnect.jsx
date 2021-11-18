e8483d441e08ce527d7aa6623c70479116ff3ca2
Jack Mkimbo <jackmkimbo@gmail.com>
2021-11-18 17:55:53 +0300

    fixes

--- src/pages/dasboard/bn_connect/BnConnect.jsx
+++ src/pages/dasboard/bn_connect/BnConnect.jsx
@@ -18,6 +18,7 @@ import UpdateComment from './scroll/comment/UpdateComment';
 import CreatePostModal from './scroll/CreatePost';
 import UpdatePostModal from './scroll/UpdatePost';
 import SkeletonCreateScrollCard from './skeleton/SkeletonCreateScrollCard';
+import SkeletonScrollCard from './skeleton/SkeletonScrollCard';
 import SkeletonSuggestedPeopleCard from './skeleton/SkeletonSuggestedPeopleCard';
 import SkeletonTrendingPostsCard from './skeleton/SkeletonTrendingPostCard';
 import SkeletonUserCard from './skeleton/SkeletonUserCard';
@@ -199,7 +200,7 @@ export default function BnConnect() {
                             {posts?.map((scroll) => (
                                 <Suspense
                                     key={scroll?._id}
-                                    fallback={<SkeletonCreateScrollCard />}
+                                    fallback={<SkeletonScrollCard />}
                                 >
                                     <Scroll
                                         setOpen={() =>
