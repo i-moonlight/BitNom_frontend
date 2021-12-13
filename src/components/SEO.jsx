d8fc5245f0e8169337754da1b3988071b975c9fd
thecodeinfluencer <mbenjerminne@gmail.com>
2021-12-13 21:43:26 +0300

    crypto updates

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,5 +1,5 @@
 //import { helmetJsonLdProp } from 'react-schemaorg';
-import moment from 'moment';
+// import moment from 'moment';
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
@@ -11,7 +11,7 @@ function SEO(data) {
             //script={[helmetJsonLdProp(SchemaFactory(data))]}
             htmlAttributes={{ lang: `en` }}
         >
-            <script type="application/ld+json">{SchemaFactory(data)}</script>
+            {/* <script type="application/ld+json">{SchemaFactory(data)}</script> */}
             {/* General tags */}
             <title>{title || 'BitNorm Website'}</title>
             {/*  <meta charSet="utf-8" /> */}
@@ -47,136 +47,141 @@ function SEO(data) {
 
 export default SEO;
 
-const SchemaFactory = (props) => {
-    let schema;
-    if (!props?.resource) {
-        schema = {
-            '@context': 'https://schema.org',
-            '@type': 'WebPage',
-            headline: props?.title,
-            url: props?.url,
-            author: {
-                '@type': 'Organization',
-                name: 'Bitnorm',
-                url: props?.url,
-            },
-        };
-    }
-    if (props?.resource?.__typename === 'OPost') {
-        schema = {
-            '@context': 'https://schema.org',
-            '@type': 'SocialMediaPosting',
-            '@id': props?.url,
-            datePublished: moment(props?.resource?.createdAt).format(
-                'YYYY-MM-DD'
-            ),
-            author: {
-                '@type': 'Person',
-                name: props?.resource?.author?.displayName,
-                url: `https://${location.origin}/users/${props?.resource?.author?._id}/`,
-            },
-            publisher: {
-                '@type': 'Organization',
-                name: 'Bitnorm',
-                url: location.origin,
-            },
-            image: {
-                '@type': 'ImageObject',
-                url: props?.image || null,
-            },
-            text: props?.resource?.content,
-            headline: props?.resource?.content?.substring(0, 15),
-            sharedContent: props?.resource?.shared_resource?._id
-                ? {
-                      '@type': 'WebPage',
-                      headline:
-                          props?.resource?.shared_resource?.type === 'post'
-                              ? props?.resource?.shared_resource?._id?.content?.substring(
-                                    0,
-                                    15
-                                )
-                              : props?.resource?.shared_resource?._id?.title,
-                      url:
-                          props?.resource?.shared_resource?.type === 'post'
-                              ? `https://${location.origin}/posts/${props?.resource?.shared_resource?._id?._id}`
-                              : `https://${location.origin}/events/${props?.resource?.shared_resource?._id?._id}`,
-                      author: {
-                          '@type': 'Person',
-                          name:
-                              props?.resource?.shared_resource?.type === 'post'
-                                  ? props?.resource?.shared_resource?._id
-                                        ?.author?.displayName
-                                  : props?.resource?.shared_resource?._id?.host
-                                        ?.displayName,
-                          url:
-                              props?.resource?.shared_resource?.type === 'post'
-                                  ? `https://${location.origin}/users/${props?.resource?.shared_resource?._id?.author?._id}/`
-                                  : `https://${location.origin}/users/${props?.resource?.shared_resource?._id?.host?._id}/`,
-                      },
-                  }
-                : null,
-        };
-    }
-    if (props?.resource?.__typename === 'OEvent') {
-        if (props?.resource?.location?.type === 'physical') {
-            schema = {
-                '@context': 'https://schema.org',
-                '@type': 'Event',
-                name: props?.resource?.title,
-                startDate: moment(props?.resource?.startDate).format(
-                    'YYYY-MM-DDTHH:mm:ssZ'
-                ),
-                endDate: moment(props?.resource?.endDate).format(
-                    'YYYY-MM-DDTHH:mm:ssZ'
-                ),
-                eventAttendanceMode:
-                    'https://schema.org/OfflineEventAttendanceMode',
-                eventStatus: 'https://schema.org/EventScheduled',
-                image: {
-                    '@type': 'ImageObject',
-                    url: props?.image,
-                },
-                location: {
-                    '@type': 'Place',
-                    name: props?.resource?.location?.address,
-                },
-                description: props?.description,
-                publisher: {
-                    '@type': 'Organization',
-                    name: 'Bitnorm',
-                    url: location.origin,
-                },
-            };
-        } else {
-            schema = {
-                '@context': 'https://schema.org',
-                '@type': 'Event',
-                name: props?.resource?.title,
-                startDate: moment(props?.resource?.startDate).format(
-                    'YYYY-MM-DDTHH:mm:ssZ'
-                ),
-                endDate: moment(props?.resource?.endDate).format(
-                    'YYYY-MM-DDTHH:mm:ssZ'
-                ),
-                eventAttendanceMode:
-                    'https://schema.org/OnlineEventAttendanceMode',
-                eventStatus: 'https://schema.org/EventScheduled',
-                location: {
-                    '@type': 'VirtualLocation',
-                    url: props?.resource?.link,
-                },
-                image: {
-                    '@type': 'ImageObject',
-                    url: props?.image,
-                },
-                description: props?.description,
-                publisher: {
-                    '@type': 'Organization',
-                    name: 'Bitnorm',
-                    url: location.origin,
-                },
-            };
-        }
-    }
-    return JSON.stringify(schema);
-};
+// const SchemaFactory = (props) => {
+
+//     let schema;
+
+//     if (!props?.resource) {
+//         schema = {
+//             '@context': 'https://schema.org',
+//             '@type': 'WebPage',
+//             headline: props?.title,
+//             url: props?.url,
+//             author: {
+//                 '@type': 'Organization',
+//                 name: 'Bitnorm',
+//                 url: props?.url,
+//             },
+//         };
+//     }
+
+//     if (props?.resource?.__typename === 'OPost') {
+//         schema = {
+//             '@context': 'https://schema.org',
+//             '@type': 'SocialMediaPosting',
+//             '@id': props?.url,
+//             datePublished: moment(props?.resource?.createdAt).format(
+//                 'YYYY-MM-DD'
+//             ),
+//             author: {
+//                 '@type': 'Person',
+//                 name: props?.resource?.author?.displayName,
+//                 url: `https://${location.origin}/users/${props?.resource?.author?._id}/`,
+//             },
+//             publisher: {
+//                 '@type': 'Organization',
+//                 name: 'Bitnorm',
+//                 url: location.origin,
+//             },
+//             image: {
+//                 '@type': 'ImageObject',
+//                 url: props?.image || null,
+//             },
+//             text: props?.resource?.content,
+//             headline: props?.resource?.content?.substring(0, 15),
+//             sharedContent: props?.resource?.shared_resource?._id
+//                 ? {
+//                       '@type': 'WebPage',
+//                       headline:
+//                           props?.resource?.shared_resource?.type === 'post'
+//                               ? props?.resource?.shared_resource?._id?.content?.substring(
+//                                     0,
+//                                     15
+//                                 )
+//                               : props?.resource?.shared_resource?._id?.title,
+//                       url:
+//                           props?.resource?.shared_resource?.type === 'post'
+//                               ? `https://${location.origin}/posts/${props?.resource?.shared_resource?._id?._id}`
+//                               : `https://${location.origin}/events/${props?.resource?.shared_resource?._id?._id}`,
+//                       author: {
+//                           '@type': 'Person',
+//                           name:
+//                               props?.resource?.shared_resource?.type === 'post'
+//                                   ? props?.resource?.shared_resource?._id
+//                                         ?.author?.displayName
+//                                   : props?.resource?.shared_resource?._id?.host
+//                                         ?.displayName,
+//                           url:
+//                               props?.resource?.shared_resource?.type === 'post'
+//                                   ? `https://${location.origin}/users/${props?.resource?.shared_resource?._id?.author?._id}/`
+//                                   : `https://${location.origin}/users/${props?.resource?.shared_resource?._id?.host?._id}/`,
+//                       },
+//                   }
+//                 : null,
+//         };
+//     }
+
+//     if (props?.resource?.__typename === 'OEvent') {
+//         if (props?.resource?.location?.type === 'physical') {
+//             schema = {
+//                 '@context': 'https://schema.org',
+//                 '@type': 'Event',
+//                 name: props?.resource?.title,
+//                 startDate: moment(props?.resource?.startDate).format(
+//                     'YYYY-MM-DDTHH:mm:ssZ'
+//                 ),
+//                 endDate: moment(props?.resource?.endDate).format(
+//                     'YYYY-MM-DDTHH:mm:ssZ'
+//                 ),
+//                 eventAttendanceMode:
+//                     'https://schema.org/OfflineEventAttendanceMode',
+//                 eventStatus: 'https://schema.org/EventScheduled',
+//                 image: {
+//                     '@type': 'ImageObject',
+//                     url: props?.image,
+//                 },
+//                 location: {
+//                     '@type': 'Place',
+//                     name: props?.resource?.location?.address,
+//                 },
+//                 description: props?.description,
+//                 publisher: {
+//                     '@type': 'Organization',
+//                     name: 'Bitnorm',
+//                     url: location.origin,
+//                 },
+//             };
+//         } else {
+//             schema = {
+//                 '@context': 'https://schema.org',
+//                 '@type': 'Event',
+//                 name: props?.resource?.title,
+//                 startDate: moment(props?.resource?.startDate).format(
+//                     'YYYY-MM-DDTHH:mm:ssZ'
+//                 ),
+//                 endDate: moment(props?.resource?.endDate).format(
+//                     'YYYY-MM-DDTHH:mm:ssZ'
+//                 ),
+//                 eventAttendanceMode:
+//                     'https://schema.org/OnlineEventAttendanceMode',
+//                 eventStatus: 'https://schema.org/EventScheduled',
+//                 location: {
+//                     '@type': 'VirtualLocation',
+//                     url: props?.resource?.link,
+//                 },
+//                 image: {
+//                     '@type': 'ImageObject',
+//                     url: props?.image,
+//                 },
+//                 description: props?.description,
+//                 publisher: {
+//                     '@type': 'Organization',
+//                     name: 'Bitnorm',
+//                     url: location.origin,
+//                 },
+//             };
+//         }
+//     }
+
+//     return JSON.stringify(schema);
+// };
--- src/pages/dasboard/notifications/NotificationListItem.jsx
+++ src/pages/dasboard/notifications/NotificationListItem.jsx
@@ -8,7 +8,6 @@ import {
     IconButton,
     Typography,
 } from '@mui/material';
-//import moment from 'moment';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
